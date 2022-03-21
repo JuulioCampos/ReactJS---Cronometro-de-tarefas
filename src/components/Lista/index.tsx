25fce3172428aa8e7306aea2da1f2b8bcaa7de6f
Julio <julio.campos@iebtdigital.com.br>
2022-03-21 10:06:08 -0300

    criando uma lista de tarefas

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Item from './Item';
+import style from './Lista.module.scss';
+
+function Lista() {
+  let [tarefas, setTarefas] = useState([{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Javascript',
+    tempo: '01:00:00'
+  }, {
+    tarefa: "Typescript",
+    tempo: "03:00:00"
+  }]);
+  return (
+    <aside className={style.listaTarefas}>
+      <h2 onClick={()=> {
+        setTarefas([...tarefas, {tarefa: "estudar estado",  tempo: "02:00:00"}]);
+      }}> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item
+            key={index}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;
